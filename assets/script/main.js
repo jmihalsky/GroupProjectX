7fccafa09f5a22200d57b7469183f662e884a2c2
casekercan <casekercan@gmail.com>
2018-10-03 21:20:33 -0700

    updated errors based on empty fields

--- assets/script/main.js
+++ assets/script/main.js
@@ -22,44 +22,45 @@ const btnLogout = $("#logoutBtn");
 const btnNewUser = $("#newUserBtn");
 
 // Login button action - no such button - created for login
-btnLogin.on("click", function(e) { 
-  e.preventDefault();
-  var x = $(".form-control").val();
-if (x == "") {
-    alert('Please sign in or create an account to use "The Catch"');
-} else {
-    const emailTxt = $("#emailInput").val().trim();
-    const passwordTxt = $("#passwordInput").val().trim();
-    const email = emailTxt;
-    const pass = passwordTxt;
-    const auth = firebase.auth();
-    if ($("#createAccount").is(":checked")) {
-        //creates a new account with username and password
-        console.log("I checked box");
-        const promise = auth.createUserWithEmailAndPassword(email, pass);
-        promise.catch(e => console.log(e.message));
-        display();
-        
-       
-      } else {
-          console.log("i did not check box");
-          //logs in with existing user
-          const promise = auth.signInWithEmailAndPassword(email, pass);
-          promise.catch(e => console.log(e.message));
-          display();
-          }
-  //removes login widget to show stats
+btnLogin.on("click", function (e) {
+    e.preventDefault();
+    var x = $(".form-email").val();
+    var y = $(".form-password").val();
+    if ((x == "") || (y == 0)) {
+        alert('Please sign in or create an account to use "The Catch"');
+    } else {
+        const emailTxt = $("#emailInput").val().trim();
+        const passwordTxt = $("#passwordInput").val().trim();
+        const email = emailTxt;
+        const pass = passwordTxt;
+        const auth = firebase.auth();
+        if ($("#createAccount").is(":checked")) {
+            //creates a new account with username and password
+            console.log("I checked box");
+            const promise = auth.createUserWithEmailAndPassword(email, pass);
+            promise.catch(e => console.log(e.message));
+            display();
 
-}
+
+        } else {
+            console.log("i did not check box");
+            //logs in with existing user
+            const promise = auth.signInWithEmailAndPassword(email, pass);
+            promise.catch(e => console.log(e.message));
+            display();
+        }
+        //removes login widget to show stats
+
+    }
 });
-function display(){
+function display() {
     $("#login").css("display", "none");
     $("#main").css("display", "block");
 };
 
 
 // No logout button yet - hidden until signed in... - id has to be logoutBtn
-btnLogout.on("click", function(e){
+btnLogout.on("click", function (e) {
     e.preventDefault();
     firebase.auth().signOut();
     $("#login").css("display", "block");
@@ -70,14 +71,14 @@ btnLogout.on("click", function(e){
 
 // Listen for change in authentication state
 auth.onAuthStateChanged(firebaseUser => {
-    if (firebaseUser){
-       console.log(firebaseUser);
-        $("#logoutBtn").css("display","block");
+    if (firebaseUser) {
+        console.log(firebaseUser);
+        $("#logoutBtn").css("display", "block");
         $("#login").css("display", "none");
         $("#main").css("display", "block");
-    } else{
-       console.log("not logged in");
-        $("#logoutBtn").css("display","none");
+    } else {
+        console.log("not logged in");
+        $("#logoutBtn").css("display", "none");
         $("#login").css("display", "block");
         $("#main").css("display", "none");
     }
