48dc9a617aaecbaf1c7dd771ee6e2f021783d8c9
Kamran Sahrakar <41097862+ksahrakar@users.noreply.github.com>
2018-10-06 21:01:47 -0700

    not sure what was new

--- assets/script/api2.js
+++ assets/script/api2.js
@@ -19,15 +19,12 @@ $(function () {
         type: "GET",
         data: "{body}",
     }).done(function (data) {
-        //console.log(data);
+
         for (var i = 0; i < data.length; i++) {
             homeTeam = (data[i].HomeTeamName);
             awayTeam = (data[i].AwayTeamName);
             homeTeamOdds = (data[i].PregameOdds["0"].HomeMoneyLine);
             awayTeamOdds = (data[i].PregameOdds["0"].AwayMoneyLine);
-
-            // console.log(homeTeam + ":" + homeTeamOdds);
-            // console.log(awayTeam + ":" + awayTeamOdds);
             game_odds_key = awayTeam+"|"+homeTeam;
             var odds = awayTeamOdds+":"+homeTeamOdds;
             console.log(game_odds_key+" "+odds);
