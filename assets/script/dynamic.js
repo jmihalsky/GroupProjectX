80b010b69902b8c023ed78269ad36cca628c3c06
jmihalsky <jmihalsky@gmail.com>
2018-10-09 01:30:24 -0700

    completed radio buttons and switched teams around on display

--- assets/script/dynamic.js
+++ assets/script/dynamic.js
@@ -78,20 +78,43 @@ function renderPicks(doc) {
     away.attr("id", doc.game_away_alias);
 
     titlep.html("<strong>GAME #" + title + "</strong>")
-    gameNames.html(homeName + " <em>VS</em> " + awayName);
+    gameNames.html(awayName + " <em>VS</em> " + homeName);
     div.attr("class", "newSection");
-    records.html("Record: " + hAbv + "   |   Record: " + aAbv);
+    records.html("Record: " + aAbv + "   |   Record: " + hAbv);
 
+    // add radio buttons for user to pick winning team
     var usr_picks = $("<div>");
     usr_picks.addClass("game" + doc.game_number + "pick");
+
+    var pck_in_a = $("<input>");
+    pck_in_a.attr("type","radio");
+    pck_in_a.attr("name", doc.game_number);
+    pck_in_a.attr("id", doc.game_away_alias);
+
+    var pck_lbl_a = $("<label>");
+    pck_lbl_a.attr("for", doc.game_number);
+    pck_lbl_a.html(doc.game_away_name);
+
+    var pck_in_b = $("<input>");
+    pck_in_b.attr("type","radio");
+    pck_in_b.attr("name", doc.game_number);
+    pck_in_b.attr("id", doc.game_home_alias);
+
+    var pck_lbl_b = $("<label>");
+    pck_lbl_b.attr("for", doc.game_number);
+    pck_lbl_b.html(doc.game_home_name);
+
+    usr_picks.append(pck_in_a, pck_lbl_a, pck_in_b,pck_lbl_b);
+
 
 
 
     div.append(titlep);
-    div.append(home);
     div.append(away);
+    div.append(home);
     div.append(gameNames);
     div.append(records);
+    div.append(usr_picks);
 
     if (curWeek == divEmpty) {
         div.append(odds);
