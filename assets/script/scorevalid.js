ae14a9cfaf5e4882688c7910be0960cbe4658aea
jmihalsky <jmihalsky@gmail.com>
2018-10-10 14:57:52 -0700

    scoring logic

--- assets/script/scorevalid.js
+++ assets/script/scorevalid.js
@@ -1,15 +1,53 @@
 var week_id = "week05";
 var week_num = "5";
+var lstore = "";
+var game_winner = "";
 
 function give_points(){
     api_score_assembler();
-    api_wky_scores();
+    // api_wky_scores();
+    user_points();
+
+    Storage.prototype.getObj = function(key){
+        return JSON.parse(this.getItem(key))
+    };
+
+    lstore = JSON.parse(sessionStorage.getItem("gameresults"));
     
-    db.collection("usr_picks").where("week", "==", week_id).get().then((snapshot) => {
-        snapshot.docs.forEach(doc => {
-            console.log(doc.data());
-        })
-    });
+    for(var i = 0; i < lstore.length; i++)
+    {
+        var game_ctl = lstore[i].game;
+        game_winner = lstore[i].winner;
+
+        db.collection("usr_picks").where("game", "==", game_ctl).get().then((snapshot) => {
+            snapshot.docs.forEach(doc => {
+                console.log(doc.id, doc.data());
+                award_points(doc.id, doc.data().user_pick);
+            })
+        });
+    }
 }
 
 give_points();
+
+award_points(id, usr_pick){
+    if (game_winner == "T")
+    {
+        db.collection("user_picks").doc(id).update({
+            user_points: 1
+        })
+    }
+    else if (game_winner == usr_pick)
+    {
+        db.collection("user_picks").doc(id).update({
+            user_points: 2
+        })
+    }
+    else
+    {
+        db.colelction("user_picks").doc(id).update({
+            user_points: 0
+        })
+    }
+
+}
