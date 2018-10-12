f821c49be2950feeec7a170a0af4c9469f42133a
casekercan <casekercan@gmail.com>
2018-10-11 21:35:30 -0700

    trying to save to pull down master...

--- assets/script/scorevalid.js
+++ assets/script/scorevalid.js
@@ -1,15 +1,62 @@
-var week_id = "week05";
-var week_num = "5";
+var week_id = "week01";
+var week_num = "1";
+var lstore = [];
+var game_winner = "";
 
-function give_points(){
+function give_points() {
     api_score_assembler();
-    api_wky_scores();
-    
-    db.collection("usr_picks").where("week", "==", week_id).get().then((snapshot) => {
-        snapshot.docs.forEach(doc => {
-            console.log(doc.data());
-        })
-    });
+    // api_wky_scores();
+    user_points();
+
+    Storage.prototype.getObj = function (key) {
+        return JSON.parse(this.getItem(key))
+    };
+
+    lstore = JSON.parse(sessionStorage.getItem("gameresults"));
+
+    for (var i = 0; i < lstore.length; i++) {
+        var game_ctl = lstore[i].game;
+        game_winner = lstore[i].winner;
+
+        db.collection("usr_picks").where("game", "==", game_ctl).get().then((snapshot) => {
+            snapshot.docs.forEach(doc => {
+                console.log(doc.id, doc.data());
+                award_points(doc.id, doc.data().user_pick, doc.data().game, doc.data().week, doc.data().userid, game_winner);
+            })
+        });
+    }
 }
 
 give_points();
+
+function award_points(id, usr_pick, gamea, weeka, userida, gw) {
+    if (gw == "T") {
+        db.collection("user_picks").doc(id).set({
+            game: gamea,
+            user_pick: usr_pick,
+            user_points: 1,
+            user_id: userida,
+            week: weeka
+
+        })
+    }
+    else if (gw == usr_pick) {
+        db.collection("user_picks").doc(id).set({
+            game: gamea,
+            user_pick: usr_pick,
+            user_points: 2,
+            user_id: userida,
+            week: weeka
+        })
+    }
+    else {
+        db.collection("user_picks").doc(id).set({
+            game: gamea,
+            user_pick: usr_pick,
+            user_points: 0,
+            user_id: userida,
+            week: weeka
+        })
+    }
+
+}
